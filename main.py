3648e41b04f9ab8209a2b0ab10f83bb37c5dbd13
allan3prez <30559059+allan3prez@users.noreply.github.com>
2021-09-24 16:00:57 -0400

    Beginning of project to process radiosonde data

---
+++ main.py
@@ -0,0 +1,3 @@
+print("This is the beginning of the project")
+# TODO: Check in Metpy for specific radiosonde processing
+# TODO: Class that makes an object for each measurement from the radiosonde csv of type radiosonde
